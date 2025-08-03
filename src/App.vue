9634e241657face514615a2a7c6f95a86a4d113b
jpwol <jpwolfe1981@gmail.com>
2025-08-03 01:50:09 -0400

    update

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="shadow" :style="{
+    left: mouseX + 'px',
+    top: mouseY + 'px',
+  }" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mouseX: 0,
+      mouseY: 0,
+    };
+  },
+  methods: {
+    handleMouseMove(e) {
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
+    },
+  },
+  mounted() {
+    window.addEventListener("mousemove", this.handleMouseMove);
+  },
+  beforeUnmount() {
+    window.removeEventListener("mousemove", this.handleMouseMove);
+  },
+};
+</script>
