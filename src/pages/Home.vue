c5c8442fc178d8506741a7dd9f555bb48aacb618
jpwol <jpwolfe1981@gmail.com>
2025-08-04 03:31:12 -0400

    fixed layout for mobile

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="relative text-white lg:flex">
     <div
-      class="w-full lg:fixed lg:left-20 lg:h-screen lg:w-1/2 p-10 z-10 flex flex-col justify-between"
+      class="w-full lg:fixed lg:left-20 lg:h-screen lg:w-1/2 lg:p-10 md:p-10 p-5 z-10 flex flex-col justify-between"
     >
-      <header class="space-y-4 lg:mt-20">
-        <h1 class="text-5xl font-bold">Josh Wolfe</h1>
-        <h2 class="text-2xl text-gray-300">Low-Level Software Engineer</h2>
+      <header class="lg:space-y-4 space-y-2 lg:mt-20">
+        <h1 class="lg:text-5xl text-3xl font-bold">Josh Wolfe</h1>
+        <h2 class="lg:text-2xl text-xl text-gray-300">
+          Low-Level Software Engineer
+        </h2>
         <a
           href="https://wilkes.edu"
           target="_blank"
           rel="noopener noreferrer"
-          class="text-xl text-sky-300 hover:text-white transform transition-all duration-300 hover:text-2xl"
+          class="lg:text-xl text-xl text-sky-300 hover:text-white transform transition-all duration-300 hover:text-2xl"
           >Wilkes University</a
         >
       </header>
-      <div class="flex space-x-4 lg:mb-20 md:pt-10 sm-pt-10">
+      <div class="flex space-x-4 lg:mb-20 pt-10">
         <a href="https://github.com/jpwol" target="_blank" rel="noopener">
           <img
             src="../assets/github.svg"
@@ -47,7 +49,7 @@
       </div>
     </div>
 
-    <div class="lg:ml-[50%] p-10">
+    <div class="lg:ml-[50%] lg:p-10 md:pl-10 md:pr-10 pl-5 pr-5">
       <section class="lg:max-w-xl space-y-6 lg:pt-14 pb-6">
         <h1 class="text-2xl font-bold pt-6 pb-2">About Me</h1>
         <p>
@@ -95,6 +97,35 @@
 
       <section class="max-w-xl space-y-6">
         <h1 class="text-2xl font-bold pt-6 pb-2">History</h1>
+        <p>
+          Sed ut perspiciatis, unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit,
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+          eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est,
+          qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit,
+          sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et
+          dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam,
+          quis nostrum[d] exercitationem ullam corporis suscipit laboriosam,
+          nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum
+          i[r]ure reprehenderit, qui in ea voluptate velit esse, quam nihil
+          molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas
+          nulla pariatur? At vero eos et accusamus et iusto odio dignissimos
+          ducimus, qui blanditiis praesentium voluptatum deleniti atque
+          corrupti, quos dolores et quas molestias excepturi sint, obcaecati
+          cupiditate non provident, similique sunt in culpa, qui officia
+          deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
+          quidem reru[d]um facilis est e[r]t expedita distinctio. Nam libero
+          tempore, cum soluta nobis est eligendi optio, cumque nihil impedit,
+          quo minus id, quod maxime placeat facere possimus, omnis voluptas
+          assumenda est, omnis dolor repellend[a]us. Temporibus autem quibusdam
+          et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et
+          voluptates repudiandae sint et molestiae non recusandae. Itaque earum
+          rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+          maiores alias consequatur aut perferendis doloribus asperiores
+          repellat.
+        </p>
       </section>
     </div>
   </div>
