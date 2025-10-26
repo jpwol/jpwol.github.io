992ae8f76c848ffc5b601be3a08e14b552989d57
jpwol <jpwolfe1981@gmail.com>
2025-10-26 00:55:41 -0400

    style(nav): changed style of nav items
    items now fit with the thorn.nvim theme

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,15 @@
   <nav
     class="lg:fixed top-0 w-full z-50 bg-none text-white p-4 flex justify-between items-around"
   >
-    <div class="text-xl font-bold pr-5">{{ pageTitle }}</div>
+    <div class="text-xl font-bold pr-5 text-t-orange">
+      {{ pageTitle }}
+    </div>
 
     <div class="flex space-x-6">
-      <router-link to="/" class="hover:underline">Home</router-link>
-      <router-link to="/projects" class="hover:underline">Projects</router-link>
-      <router-link to="/blog" class="hover:underline">Blog</router-link>
-      <router-link to="/contact" class="hover:underline">Contact</router-link>
+      <router-link to="/" class="navItem">[ Home ]</router-link>
+      <router-link to="/projects" class="navItem">[ Projects ]</router-link>
+      <router-link to="/blog" class="navItem">[ Blog ]</router-link>
+      <router-link to="/contact" class="navItem">[ Contact ]</router-link>
     </div>
   </nav>
 </template>
@@ -21,7 +23,21 @@ const route = useRoute();
 const pageTitle = computed(() => route.meta.title);
 </script>
 
-<style>
+<style scoped>
+.navItem {
+  color: var(--color-t-cyan);
+  font-weight: bold;
+
+  transition:
+    transform 0.2s ease,
+    color 0.2s ease;
+}
+
+.navItem:hover {
+  color: var(--color-t-lightgreen);
+  transform: translateX(-2px);
+}
+
 .fade-slide-enter-active,
 .fade-slide-leave-active {
   transition:
