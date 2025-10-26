55dcdb9bac9098951802dc2c36985a2b97d2c7bf
jpwol <jpwolfe1981@gmail.com>
2025-10-26 02:23:23 -0400

    style(app): added page animations

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div>
-    <!-- <CursorShadow /> -->
     <Nav v-if="showNavbar" />
-    <router-view />
+    <transition name="page" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script setup>
 import Nav from "./components/Nav.vue";
-// import CursorShadow from "./components/CursorShadow.vue";
 import { useRoute } from "vue-router";
 import { computed } from "vue";
 
 const route = useRoute();
 const showNavbar = computed(() => route.path != "/");
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 400ms ease;
+}
+
+.page-enter-from {
+  opacity: 0;
+  transform: translateX(10px);
+}
+
+.page-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.page-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(-10px);
+}
+</style>
