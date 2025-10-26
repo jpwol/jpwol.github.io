2219e5e79826149d5c0280107652c5995f5a3952
jpwol <jpwolfe1981@gmail.com>
2025-10-26 02:22:38 -0400

    fix(nav): fixed display for smaller screens
    it was the page title this time

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,11 +2,11 @@
   <nav
     class="lg:fixed top-0 w-full z-50 bg-none p-4 flex justify-between items-around"
   >
-    <div class="text-xl font-bold pr-5 text-t-orange">
+    <div class="relative lg:text-xl top-1 text-sm font-bold pr-5 text-t-orange">
       {{ pageTitle }}
     </div>
 
-    <div class="flex lg:space-x-6 space-x-4 lg:text-lg lg:mt-0 mt-1 text-sm">
+    <div class="flex lg:space-x-6 space-x-3 lg:text-lg lg:mt-0 mt-1 text-sm">
       <router-link to="/" class="navItem">[ Home ]</router-link>
       <router-link to="/projects" class="navItem">[ Projects ]</router-link>
       <router-link to="/blog" class="navItem">[ Blog ]</router-link>
