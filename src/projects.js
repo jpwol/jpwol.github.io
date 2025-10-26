c3a11d83335a712a59e9752db498d4be44eb4dc9
jpwol <jpwolfe1981@gmail.com>
2025-10-26 00:57:34 -0400

    feat(projects): project structs now contain lang icon link
    icon member links to cdn svg logo for dynamically loading language icons
    per project

--- src/projects.js
+++ src/projects.js
@@ -3,12 +3,14 @@ export default [
     name: "thorn.nvim",
     slug: "thorn",
     description: "A light and dark green colorscheme for Neovim, with extras",
+    icon: "https://cdn.simpleicons.org/lua",
     component: () => import("./pages/ProjectPages/Thorn.vue"),
   },
   {
     name: "bitwise-cli",
     slug: "bitwisecli",
     description: "A command-line arithematic REPL with bitwise support.",
+    icon: "https://cdn.simpleicons.org/zig",
     component: () => import("./pages/ProjectPages/Bitwise.vue"),
   },
 ];
